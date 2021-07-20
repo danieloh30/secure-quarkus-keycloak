a89e6fd5ab0328af0be9ebbfc58b10b3bacafe70
danieloh30 <doh@redhat.com>
2021-07-20 18:04:45 -0400

    first commit

---
+++ src/main/java/org/acme/UsersResource.java
@@ -0,0 +1,25 @@
+package org.acme;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+@Path("/api/users")
+public class UsersResource {
+
+    @Inject
+    SecurityIdentity identity;
+
+    @GET
+    @Path("/me")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User me() {
+        return new User(identity);
+    }
+
+}
